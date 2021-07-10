995e15545ad802870ab1aa8596cf749869aef48d
Silvio <silvio.dias@etec.sp.gov.br>
2021-07-09 23:51:41 -0300

    definindo cores

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export default {
+  backgroundColor: '#252525',
+  backgroundCard: '#ebebeb',
+  title: '#404040',
+  description: '#545454',
+  icon: '#9fadc4',
+  active: '#4277cf',
+  message: '#b5b5b5'
+}
